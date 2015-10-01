f1500a73ea396c03c6957f5c7a7fc439dd4c52d1
Adam Rackis <adamrackis@gmail.com>
2015-10-01 16:54:16 -0500

    adding initial transpile task so transpiled files can be generated after a fresh git clone (or after a git pull if you don't have the main transpiler running)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	mocha = require('gulp-mocha'),
+	gprint = require('gulp-print'),
+	babel = require('gulp-babel');
+
+gulp.task('initial-transpile', function () {
+	gulp.src('**/**-es6.js')
+		.pipe(babel({ stage: 1 }))
+		.pipe(rename(function (path) {
+			path.basename = path.basename.replace(/-es6$/, '');
+		}))
+		.pipe(gulp.dest(''))
+		.pipe(gprint(function(filePath){ return "File processed: " + filePath; }));
+});
 
 gulp.task('test', function () {
     require('./testUtil/testSetup');
