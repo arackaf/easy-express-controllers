2f7022565775a48fd96cef1def5c637d00f5be42
Adam Rackis <adamrackis@hotmail.com>
2015-10-09 00:23:01 -0500

    expanding on browser manual tests

--- runTestControllers-es6.js
+++ runTestControllers-es6.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var expressController = require('./index');
-var path = require("path");
+var { easyControllers } = require('./index');
+var path = require('path');
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.listen(3001);
 
-expressController.createController(app, 'person');
+easyControllers.createController(app, 'person');
 
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/runner.htm'));
