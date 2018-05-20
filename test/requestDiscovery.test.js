52c14ba2a5af8519309c3df83f8bbbb5471678d6
Adam Rackis <adamrackis@hotmail.com>
2018-05-20 15:52:48 -0500

    rest of tests converted

---
+++ test/requestDiscovery.test.js
@@ -0,0 +1,39 @@
+const { start, stop } = require("../testUtil/testSetup");
+
+beforeAll(() => {
+  start();
+});
+afterAll(() => {
+  stop();
+});
+
+describe("Manual http packets work", function() {
+  beforeAll(function() {
+    app.get("/manualGet", function(request, response) {
+      response.send({ result: "Success", x: request.body.x });
+    });
+
+    app.post("/manualPost", function(request, response) {
+      response.send({ result: "Success", x: request.body.x });
+    });
+  });
+
+  it("gets properly", function(done) {
+    request.get("http://localhost:3000/manualGet", { form: { x: 12 } }, function(error, response, obj) {
+      obj = JSON.parse(obj);
+      assert.equal("Success", obj.result);
+      assert.equal(12, obj.x);
+      done();
+    });
+  });
+
+  it("posts properly", function(done) {
+    request.post("http://localhost:3000/manualPost", { form: { x: 12 } }, function(error, response, obj) {
+      obj = JSON.parse(obj);
+
+      assert.equal("Success", obj.result);
+      assert.equal(12, obj.x);
+      done();
+    });
+  });
+});
