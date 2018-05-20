712ab413cc10f4db1ce9436c8e537ee969a55eb6
Adam Rackis <adamrackis@hotmail.com>
2018-05-20 16:39:07 -0500

    Clean up dummy controller layout.  Remove un-needed dependencies

--- test/createAllControllers.test.js
+++ test/createAllControllers.test.js
@@ -1,6 +1,3 @@
-const sinon = require("sinon");
-const proxyquire = require("proxyquire");
-
 const { start, stop } = require("../testUtil/testSetup");
 jest.mock("../util/createController");
 
@@ -14,7 +11,7 @@ afterAll(() => {});
 describe("creare all controllers tests", function() {
   it("should create a single controller", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest1" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest1" };
 
     createAllControllers(-1, null, overrides);
     let spy = require("../util/createController");
@@ -23,7 +20,7 @@ describe("creare all controllers tests", function() {
 
   it("should skip ES6 files configured", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest2" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest2" };
 
     createAllControllers(-1, { fileTest: f => !/-es6\.js$/i.test(f) }, overrides);
     let spy = require("../util/createController");
@@ -32,7 +29,7 @@ describe("creare all controllers tests", function() {
 
   it("should skip ES6 files by default with file extension", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest3" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest3" };
 
     createAllControllers(-1, null, overrides);
     let spy = require("../util/createController");
@@ -41,7 +38,7 @@ describe("creare all controllers tests", function() {
 
   it("should work with 2 levels", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest4" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest4" };
 
     createAllControllers(-1, null, overrides);
 
@@ -52,7 +49,7 @@ describe("creare all controllers tests", function() {
 
   it("should work with 3 levels", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest5" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest5" };
 
     createAllControllers(-1, null, overrides);
 
@@ -70,7 +67,7 @@ describe("creare all controllers tests", function() {
 
   it("should work with 3 levels and properly exclude es6 files", function() {
     var createAllControllers = require("../util/createAllControllers");
-    var overrides = { controllerPath: "./createAllControllersTest6" };
+    var overrides = { controllerPath: "./dummyControllers/createAllControllersTest6" };
 
     createAllControllers(-1, { fileTest: f => !/-es6\.js$/i.test(f) }, overrides);
 
