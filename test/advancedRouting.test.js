4ca27994872812f38a5ecac18cad8eb71ecd74d3
Adam Rackis <adamrackis@hotmail.com>
2018-05-20 14:52:17 -0500

    first set of tests converted

---
+++ test/advancedRouting.test.js
@@ -0,0 +1,35 @@
+const { start, stop } = require("../testUtil/testSetup");
+
+beforeAll(() => {
+  start();
+});
+afterAll(() => {
+  stop();
+});
+
+describe("Advanced routing tests", function() {
+  it("routes default get paths", function(done) {
+    utils.getAndCheck("http://localhost:3000/globalRoute1", {}, done, obj => {
+      console.log("RECEIVED", obj);
+      assert.isTrue(obj.received);
+    });
+  });
+
+  it("rejects default get paths that was overridden", function(done) {
+    utils.verbsAreRejected("http://localhost:3000/globalcontroller/foo", done, ["get"]);
+  });
+
+  utils.checkRoutesAndVerbs("http://localhost:3000/globalRoute2", ["post", "put"]);
+
+  it("rejects default get paths that was overridden 2", function(done) {
+    utils.verbsAreRejected("http://localhost:3000/globalcontroller/foo", done, ["get"]);
+  });
+
+  it("rejects opted-in verbs on original paths that was overridden", function(done) {
+    utils.verbsAreRejected("http://localhost:3000/globalcontroller/lala", done, ["get", "post", "put", "delete"]);
+  });
+
+  it("rejects opted-in verbs on original paths that was overridden 2", function(done) {
+    utils.verbsAreRejected("http://localhost:3000/globalcontroller/lolo", done, ["get", "post", "put", "delete"]);
+  });
+});
--- testUtil/testSetup.js
+++ testUtil/testSetup.js
@@ -1,117 +1,147 @@
-var express = require('express');
-var app = express();
-var assert = require('chai').assert;
-var request = require('request');
-var bodyParser = require('body-parser');
-var expressController = require('../index');
-var cors = require('cors');
+var express = require("express");
+var assert = require("chai").assert;
+var request = require("request");
+var bodyParser = require("body-parser");
+var expressController = require("../index");
+var cors = require("cors");
+var app;
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
-app.use(cors());
+function start() {
+  app = express();
+  app.use(bodyParser.json()); // to support JSON-encoded bodies
+  app.use(
+    bodyParser.urlencoded({
+      // to support URL-encoded bodies
+      extended: true
+    })
+  );
+  app.use(cors());
 
-global.server = app.listen(3000);
+  global.server = app.listen(3000);
 
-global.app = app;
-global.request = request;
-global.expressController = expressController;
-global.easyControllers = expressController.easyControllers;
-global.assert = assert;
+  global.app = app;
+  global.request = request;
+  global.expressController = expressController;
+  global.assert = assert;
+  global.easyControllers = expressController.easyControllers;
+  easyControllers.createController(app, "books/Foo", { __dirname: __dirname, controllerPath: "../controllers2" });
+  easyControllers.createController(app, "Person");
+  easyControllers.createController(app, "NewRouting");
+  easyControllers.createController(app, "NewRouting2");
+  easyControllers.createController(app, "NewRouting3");
+  easyControllers.createController(app, "ExtendedDecorators");
+  easyControllers.createController(app, "GlobalController");
+  easyControllers.createController(app, "publisher/publisherDetails");
+  easyControllers.createController(app, "books/Book");
+  easyControllers.createController(app, "books/BookDefaultPost");
+}
 
+function stop() {
+  global.server.close();
+}
 //forcing a different root __dirname and custom path to controllers directory
-easyControllers.createController(app, 'books/Foo', { __dirname: __dirname, controllerPath: '../controllers2' });
 
-function postAndCheck(uri, data, done, check, options){
-    runAndCheck(uri, data, 'post', done, check);
+function postAndCheck(uri, data, done, check, options) {
+  runAndCheck(uri, data, "post", done, check);
 }
 
-function getAndCheck(uri, data, done, check, options){
-    runAndCheck(uri, data, 'get', done, check);
+function getAndCheck(uri, data, done, check, options) {
+  runAndCheck(uri, data, "get", done, check);
 }
 
-function putAndCheck(uri, data, done, check, options){
-    runAndCheck(uri, data, 'put', done, check);
+function putAndCheck(uri, data, done, check, options) {
+  runAndCheck(uri, data, "put", done, check);
 }
 
-function patchAndCheck(uri, data, done, check, options){
-    runAndCheck(uri, data, 'patch', done, check);
+function patchAndCheck(uri, data, done, check, options) {
+  runAndCheck(uri, data, "patch", done, check);
 }
 
-function deleteAndCheck(uri, data, done, check, options){
-    runAndCheck(uri, data, 'delete', done, check);
+function deleteAndCheck(uri, data, done, check, options) {
+  runAndCheck(uri, data, "delete", done, check);
 }
 
-function runAndCheck(uri, data, verb, done, check, options = { }){
-    request({
-        uri,
-        method: verb,
-        body: data,
-        json: true
-    }, function(error, response, obj){
-        check(obj);
-        done();
-    });
-}
-
-function verbsAreRejected(uri, done, verbs){
-    Promise.all(verbs.map(runVerb)).then(() => done());
-
-    function runVerb(verb){
-        return new Promise(res => {
-            let regVerb = verb;
-            if(regVerb == 'delete') regVerb = 'del';
-
-            request[regVerb](uri, {}, function (error, response, obj) {
-                assert.isTrue(new RegExp(`cannot ${verb}`, 'i').test(obj), `${uri} didn't fail for ${verb} but should have`); //this is how request handles requests for which the very is not defined....
-                res();
-            });
-        }).catch(() => null)
+function runAndCheck(uri, data, verb, done, check, options = {}) {
+  request(
+    {
+      uri,
+      method: verb,
+      body: data,
+      json: true
+    },
+    function(error, response, obj) {
+      check(obj);
+      done();
     }
+  );
 }
 
-function checkRoutesAndVerbs(uri, verbs){
-    verbs.forEach(verb => {
-        it(`routes ${verb} to ${uri.replace('http://localhost:3000', '')}`, function(done){
-            utils[`${verb}AndCheck`](uri, {}, done, obj => assert.isTrue(obj.received));
-        });
+function verbsAreRejected(uri, done, verbs) {
+  Promise.all(verbs.map(runVerb)).then(() => done());
+
+  function runVerb(verb) {
+    return new Promise(res => {
+      let regVerb = verb;
+      if (regVerb == "delete") regVerb = "del";
+
+      request[regVerb](uri, {}, function(error, response, obj) {
+        assert.isTrue(new RegExp(`cannot ${verb}`, "i").test(obj), `${uri} didn't fail for ${verb} but should have`); //this is how request handles requests for which the very is not defined....
+        res();
+      });
+    }).catch(() => null);
+  }
+}
+
+function checkRoutesAndVerbs(uri, verbs) {
+  verbs.forEach(verb => {
+    it(`routes ${verb} to ${uri.replace("http://localhost:3000", "")}`, function(done) {
+      utils[`${verb}AndCheck`](uri, {}, done, obj => assert.isTrue(obj.received));
     });
-    let badVerbs = ['get', 'post', 'put', 'delete'].filter(verb => verbs.indexOf(verb) === -1);
-    it(`rejects ${uri} with [${badVerbs}]`, function(done) {
-        utils.verbsAreRejected(uri, done, badVerbs);
-    });
+  });
+  let badVerbs = ["get", "post", "put", "delete"].filter(verb => verbs.indexOf(verb) === -1);
+  it(`rejects ${uri} with [${badVerbs}]`, function(done) {
+    utils.verbsAreRejected(uri, done, badVerbs);
+  });
 }
 
-function shallowObjEqual(obj1, obj2){
-    if (obj1 == null && obj2 == null) return true;
+function shallowObjEqual(obj1, obj2) {
+  if (obj1 == null && obj2 == null) return true;
 
-    let keys1 = Object.keys(obj1),
-        keys2 = Object.keys(obj2);
+  let keys1 = Object.keys(obj1),
+    keys2 = Object.keys(obj2);
 
-    return keys1.length == keys2.length && keys1.every(k => obj1[k] == obj2[k]);
+  return keys1.length == keys2.length && keys1.every(k => obj1[k] == obj2[k]);
 }
 
-function verifyCreateControllerSpyCalls(spy, calls){
-    assert.equal(spy.callCount, calls.length, 'total count wrong');
-    let callsToVerify = calls.map((c, i) => spy.getCall(i));
+function verifyCreateControllerSpyCalls(spy, calls) {
+  assert.equal(spy.callCount, calls.length, "total count wrong");
+  let callsToVerify = calls.map((c, i) => spy.getCall(i));
 
-    assert.isFalse(callsToVerify.some(c => c.args[0] !== app), 'Some calls missing app param');
+  assert.isFalse(callsToVerify.some(c => c.args[0] !== app), "Some calls missing app param");
 
-    //I don't pass the app value in for all calls to verify, so the args are shifted
-    callsToVerify.forEach(spyCall => assert.equal(1, calls.filter(callToVerify =>
-        callToVerify[0] == spyCall.args[1] && shallowObjEqual(callToVerify[1], spyCall.args[2])).length, `Missing call for ${spyCall.args[1]}`)
-    );
+  //I don't pass the app value in for all calls to verify, so the args are shifted
+  callsToVerify.forEach(spyCall =>
+    assert.equal(
+      1,
+      calls.filter(callToVerify => callToVerify[0] == spyCall.args[1] && shallowObjEqual(callToVerify[1], spyCall.args[2])).length,
+      `Missing call for ${spyCall.args[1]}`
+    )
+  );
 }
 
 global.utils = {
-    postAndCheck,
-    getAndCheck,
-    putAndCheck,
-    patchAndCheck,
-    deleteAndCheck,
-    runAndCheck,
-    verbsAreRejected,
-    checkRoutesAndVerbs,
-    verifyCreateControllerSpyCalls
+  postAndCheck,
+  getAndCheck,
+  putAndCheck,
+  patchAndCheck,
+  deleteAndCheck,
+  runAndCheck,
+  verbsAreRejected,
+  checkRoutesAndVerbs,
+  verifyCreateControllerSpyCalls
+};
+
+module.exports = {
+  start,
+  stop
 };
