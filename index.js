331d182ee9d18092279373e3aae93512f8f379f5
Adam Rackis <adamrackis@hotmail.com>
2018-05-20 16:06:53 -0500

    remove un-needed files

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-'use strict';
-
 var createController = require('./util/createController');
 var createAllControllers = require('./util/createAllControllers');
-
-var _require = require('./util/decorators'),
-    controller = _require.controller,
-    httpGet = _require.httpGet,
-    httpPost = _require.httpPost,
-    httpPut = _require.httpPut,
-    httpDelete = _require.httpDelete,
-    httpPatch = _require.httpPatch,
-    acceptVerbs = _require.acceptVerbs,
-    route = _require.route,
-    nonRoutable = _require.nonRoutable;
+var {
+    controller,
+    httpGet,
+    httpPost,
+    httpPut,
+    httpDelete,
+    httpPatch,
+    acceptVerbs,
+    route,
+    nonRoutable
+} = require('./util/decorators');
 
 var easyControllers = {
-    createController: createController,
-    createAllControllers: createAllControllers
+    createController,
+    createAllControllers
 };
 
 module.exports = {
-    createController: createController,
-    createAllControllers: createAllControllers,
-    easyControllers: easyControllers,
-    controller: controller,
-    httpGet: httpGet,
-    httpPost: httpPost,
-    httpPut: httpPut,
-    httpDelete: httpDelete,
-    httpPatch: httpPatch,
-    acceptVerbs: acceptVerbs,
-    route: route,
-    nonRoutable: nonRoutable
+    createController,
+    createAllControllers,    
+    easyControllers,
+    controller,
+    httpGet,
+    httpPost,
+    httpPut,
+    httpDelete,
+    httpPatch,
+    acceptVerbs,
+    route,
+    nonRoutable
 };
