833c10d5a19ec9d320fe61819ccf3c3ced561282
Adam Rackis <adamrackis@hotmail.com>
2018-05-20 19:53:31 -0500

    Code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 const gulp = require("gulp");
-const rename = require("gulp-rename");
-const gprint = require("gulp-print");
 const babel = require("gulp-babel");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
-const fs = require("fs");
+const gprint = require("gulp-print");
 
 gulp.task("transpile-all", function() {
   transpileFolder("controllers");
@@ -22,43 +18,3 @@ function transpileFolder(name) {
       })
     );
 }
-
-gulp.task("transpile-watch", function() {
-  return gulp.watch("**/**-es6.js", function(obj) {
-    if (obj.type === "changed") {
-      gulp
-        .src(obj.path, { base: "./" })
-        .pipe(
-          plumber({
-            errorHandler: function(error) {
-              //babel error - dev typed in in valid code
-              if (error.fileName) {
-                var fileParts = error.fileName.split("\\");
-                try {
-                  notify.onError(error.name + " in " + fileParts[fileParts.length - 1])(error);
-                } catch (e) {} //gulp-notify may break if not run in Win 8
-                console.log(error.name + " in " + error.fileName);
-              } else {
-                notify.onError("Oh snap, file system error! :(")(error);
-              }
-
-              console.log(error.message);
-              this.emit("end");
-            }
-          })
-        )
-        .pipe(babel({ plugins: ["transform-decorators-legacy"] }))
-        .pipe(
-          rename(function(path) {
-            path.basename = path.basename.replace(/-es6$/, "");
-          })
-        )
-        .pipe(gulp.dest(""))
-        .pipe(
-          gprint(function(filePath) {
-            return "File processed: " + filePath;
-          })
-        );
-    }
-  });
-});
